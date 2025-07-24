540680abc3bfda24d242f1f0e7c34241b99c6091
panidharpvs00 <120071353+panidharpsv000@users.noreply.github.com>
2025-07-24 22:42:43 +0530

    Update RoomRepository.java

--- chat-app-backend/src/main/java/com/substring/chat/repositories/RoomRepository.java
+++ chat-app-backend/src/main/java/com/substring/chat/repositories/RoomRepository.java
@@ -6,6 +6,5 @@ import com.substring.chat.entities.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, String> {
-    //get room using room id
     Room findByRoomId(String roomId);
 }
