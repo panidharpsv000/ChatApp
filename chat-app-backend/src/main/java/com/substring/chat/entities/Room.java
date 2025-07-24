7fd2ba12afe69fb2952d1014dd3b17787d493964
panidharpvs00 <120071353+panidharpsv000@users.noreply.github.com>
2025-07-24 22:38:16 +0530

    Update Room.java

--- chat-app-backend/src/main/java/com/substring/chat/entities/Room.java
+++ chat-app-backend/src/main/java/com/substring/chat/entities/Room.java
@@ -3,8 +3,11 @@ package com.substring.chat.entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -18,7 +21,8 @@ public class Room {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String roomId;
-    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JsonManagedReference
     private List<Message> messages=new ArrayList<>();
 
 
